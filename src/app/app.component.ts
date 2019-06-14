391765a3649f6c022113c605e027b756e6ef554e
703114283 <christian.knoll@universalorlando.com>
2019-06-14 11:14:28 -0400

    passing data to "like" buttons from JSON objet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 	title = 'The "Like" Button';
-	
-	movie = {
-		title: 'Needs Title',
-		description: 'Lovely little movie with that one woman who was in that other movie.',
-		likesCount: 10,
-		isLiked: true
-	}
+
+	movies = [
+		{
+			title: 'Alien',
+			description: 'In space...no one can hear you scream.',
+			likesCount: 10,
+			isLiked: false
+		},
+		{
+			title: 'The Dark Knight',
+			description: 'Batman has been able to keep a tight lid on crime in Gotham City, until the Joker comes to town.',
+			likesCount: 13,
+			isLiked: true
+		},
+		{
+			title: 'The Godfather',
+			description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+			likesCount: 25,
+			isLiked: true
+		},
+	]
 }
